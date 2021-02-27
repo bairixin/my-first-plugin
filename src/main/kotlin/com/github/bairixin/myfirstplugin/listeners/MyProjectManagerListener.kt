e305bd63e08a892d4b0869579391f044ec77f2c9
GitHub Action <action@github.com>
2021-02-27 07:51:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/bairixin/myfirstplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/bairixin/myfirstplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bairixin.myfirstplugin.listeners
 
+import com.github.bairixin.myfirstplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
